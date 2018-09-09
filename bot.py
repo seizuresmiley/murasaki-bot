e33910addb188d5fd7fe58bf8bae815f6c13e96c
seizuresmiley <shoopedlol@gmail.com>
2018-09-09 21:52:51 +0700

    fix on_ready

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
-async def ready():
+async def on_ready():
     print('Logged in.')
     print('Bot Name is')
     print(bot.user.name)
